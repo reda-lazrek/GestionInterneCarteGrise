4fd72f695a24874188cea961ad81836f89c4f2c9
reda-lazrek <123340557+reda-lazrek@users.noreply.github.com>
2023-03-04 19:44:02 +0100

    Initial commit

---
+++ src/main/java/com/example/cartegrise/GestionInterneCarteGriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.cartegrise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionInterneCarteGriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionInterneCarteGriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cartegrise/GestionInterneCarteGriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cartegrise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionInterneCarteGriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
